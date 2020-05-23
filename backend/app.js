8bf0339ec9d5986e35992ff89771530a9d93fbd8
Bruno Ricardo <brunoricardosecco@hotmail.com>
2020-05-23 18:40:23 -0300

    feat: add the graphql schema in the graphql endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/index');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+  })
+);
 
 app.listen(3001, () => {
   console.log('Opening for requests on port 3001');
